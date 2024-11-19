fbe83a97461b80b46c53e3e9662e643d2398ca0f
Andrej S. <szegeny.andrej.2004@gmail.com>
2024-11-19 21:41:47 +0100

    Update HOME.py

--- HOME.py
+++ HOME.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+def set_background():
+    st.markdown("""
+        <style>
+        .stApp {
+            background-color: #1D2B3A;
+        }
+        </style>
+        """, unsafe_allow_html=True)
+
+set_background()
+
 st.set_page_config(
     page_title = "Home"
 )
